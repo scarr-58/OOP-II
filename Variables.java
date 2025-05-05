d5dcc00ea45b9dda84daf38f4f9f251a46c19a3b
scarr-58 <thescar58@gmail.com>
2025-05-05 14:34:41 +0300

    A Variable data type that calculates interest

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    void showInterest(){
+        double amount;
+        double interest;
+
+        amount=1200.00;
+        System.out.println("amount: " + amount);
+
+        interest=3.0;
+        amount=amount * (100+interest)/100;
+        System.out.println("after a year: " + amount);
+
+        amount= amount * (100+interest)/100;
+        System.out.println("after two years: " + amount);
+    }
+       public static void main(String[] args){
+        (new Variables()).showInterest();
+    }
+    
+}
